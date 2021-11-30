19f42391f161827f4dac97549250b36ce64d1b75
haido86 <thithuhai.do@gmail.com>
2021-11-30 23:46:00 +0200

    1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import { Rating } from "@mui/material";
 
 function App() {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [value, setValue] = React.useState(2);
 
   return (
     <div>
       <h1>give feedback </h1>
-      <Button variant="outlined">Outlined</Button>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+
       <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 }
