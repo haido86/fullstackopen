a9b4678670b0bd08183f8f238f5548667526904e
haido86 <thithuhai.do@gmail.com>
2021-11-30 23:27:05 +0200

    1.6 step 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { Rating } from "@mui/material";
 
 function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [value, setValue] = React.useState(2);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback </h1>
+      <Button variant="outlined">Outlined</Button>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+      <h1>statistics</h1>
     </div>
   );
 }
