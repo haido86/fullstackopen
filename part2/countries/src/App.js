20c668d6a9c6876324367616c9d52bd34b5a9ae4
haido86 <thithuhai.do@gmail.com>
2021-12-12 00:25:02 +0200

    2.12: Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const addFilter = (event) => {
+    event.preventDefault();
+    setFilter("");
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  let countriesFilter = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filter)
   );
-}
 
+  return (
+    <>
+      <div style={{ margin: "20px" }}>find countries</div>
+      <form style={{ margin: "20px" }} onSubmit={addFilter}>
+        <input value={filter} onChange={handleFilterChange} />
+      </form>
+      <div>
+        {countriesFilter.length <= 10
+          ? countriesFilter.map((filterCountry) => (
+              <p key={filterCountry.name.common}>{filterCountry.name.common}</p>
+            ))
+          : filter.length > 0 && "Too many matches, specify another filter"}
+      </div>
+    </>
+  );
+};
 export default App;
